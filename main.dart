a6bab504eef3dfb4f64493ad8857b1ea1b907cbb
Nouman <nk012284@gmail.com>
2026-07-03 20:37:21 +0500

    Change temperature

--- main.dart
+++ main.dart
@@ -1,26 +1,30 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('a: ');
-  final a = num.parse(stdin.readLineSync()!);
+  /*     stdout.write('a: ');
+      final a = num.parse(stdin.readLineSync()!);
 
-  stdout.write('op (+ - * / ~/ %): ');
-  final op = stdin.readLineSync();
+      stdout.write('op (+ - * / ~/ %): ');
+      final op = stdin.readLineSync();
 
-  stdout.write('b: ');
-  final b = num.parse(stdin.readLineSync()!);
+      stdout.write('b: ');
+      final b = num.parse(stdin.readLineSync()!);
 
-  final r = op == '+'
-      ? a + b
-      : op == '-'
-      ? a - b
-      : op == '*'
-      ? a * b
-      : op == '/'
-      ? a / b
-      : op == '%'
-      ? a % b
-      : null;
+      final r = op == '+'
+          ? a + b
+          : op == '-'
+          ? a - b
+          : op == '*'
+          ? a * b
+          : op == '/'
+          ? a / b
+          : op == '%'
+          ? a % b
+          : null;*/
+  stdout.write("Enter temperature in Celsius: ");
+  double celsius = double.parse(stdin.readLineSync()!);
 
-  print('$a $op $b = $r');
+  double fahrenheit = (celsius * 9 / 5) + 32;
+
+  print("Temperature in Fahrenheit: $fahrenheit");
 }
